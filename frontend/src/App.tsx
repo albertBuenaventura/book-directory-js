67e686c7256400eba6d617fd203e11f266ba0c45
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2023-07-03 15:25:04 +0800

    feat: directory list page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import "./App.css";
 import { Breadcrumbs } from "./components/Breadcrumbs";
 import { Button } from "./components/Button";
+import { Directory, FileType } from "./containers/Directory/Directory";
 
 const breadcrumbs = [
   {
@@ -18,13 +19,26 @@ const breadcrumbs = [
   },
 ];
 
+const files = [
+  {
+    id: "test",
+    name: "File",
+    type: FileType.Book,
+  },
+  {
+    id: "111",
+    name: "Cabinet",
+    type: FileType.Location,
+  },
+];
+
 function App() {
   const addBook = useCallback(() => {}, []);
   const addLocation = useCallback(() => {}, []);
 
   return (
-    <div className="flex w-full h-full mt-10 justify-center">
-      <div className="flex bg-slate-200 p-6 w-11/12 max-w-7xl rounded">
+    <div className="flex flex-col w-full h-full mt-10 justify-center items-center gap-y-8">
+      <div className="flex flex-col gap-y-8 bg-slate-200 p-6 w-11/12 max-w-7xl rounded">
         <div className="flex w-full items-center gap-x-4">
           <Breadcrumbs paths={breadcrumbs} className="w-full" />
           <Button onClick={addBook} className="whitespace-nowrap">
@@ -34,6 +48,7 @@ function App() {
             Add Location
           </Button>
         </div>
+        <Directory files={files} />
       </div>
     </div>
   );
---
+++ frontend/src/containers/Directory/Directory.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import cx from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileLines, faFolder } from "@fortawesome/free-regular-svg-icons";
+import {
+  faArrowsUpDownLeftRight,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+
+export enum FileType {
+  Book,
+  Location,
+}
+
+export type File = {
+  id: string;
+  name: string;
+  type: FileType;
+};
+
+export type DirectoryProps = {
+  files: File[];
+  onClickBack?: () => void;
+  onFolderClick?: (file: File) => void;
+  onFileDelete?: (file: File) => void;
+  hideBackFolder?: boolean;
+};
+
+export function Directory({
+  files,
+  onClickBack,
+  onFolderClick,
+  onFileDelete,
+  hideBackFolder = false,
+}: DirectoryProps) {
+  return (
+    <div className="flex flex-col bg-white rounded p-6 gap-y-2">
+      {!hideBackFolder && (
+        <div
+          className="flex items-center gap-x-4 pb-1 border-0 border-b-2 hover:bg-slate-100"
+          onClick={onClickBack}
+          role="button"
+        >
+          <FontAwesomeIcon icon={faFolder} />
+          <span>...</span>
+        </div>
+      )}
+      {files.map((file) => {
+        const isLocation = file.type === FileType.Location;
+
+        return (
+          <div className="flex items-center justify-between pb-1 border-0 border-b-2 hover:bg-slate-100">
+            <div
+              className={cx("flex items-center gap-x-4", {
+                "cursor-pointer": isLocation,
+              })}
+              onClick={isLocation ? () => onFolderClick?.(file) : undefined}
+            >
+              <FontAwesomeIcon icon={isLocation ? faFolder : faFileLines} />
+              <span>{file.name}</span>
+            </div>
+            <div className="flex gap-x-4">
+              <FontAwesomeIcon icon={faArrowsUpDownLeftRight} />
+              <FontAwesomeIcon
+                icon={faTrash}
+                onClick={() => onFileDelete?.(file)}
+                role="button"
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
