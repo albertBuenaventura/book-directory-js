765a912a4d6fd88bdc686cdda49d0c4a8126ee66
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2023-07-04 12:42:29 +0800

    feat: add book or location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 import { Breadcrumbs } from "./components/Breadcrumbs";
 import { Button } from "./components/Button";
-import { Directory } from "./containers/Directory/Directory";
+import { Directory, FileType } from "./containers/Directory/Directory";
+import { AddModal } from "./components/Modals/AddModal";
+import { addBook } from "./api/books";
 
 const breadcrumbs = [
   {
@@ -22,9 +24,17 @@ const breadcrumbs = [
 
 function App() {
   const queryClient = new QueryClient();
+  const [showAddBookModal, setShowAddBookModal] = useState(false); 
+  const [showAddLocationModal, setShowAddLocationModal] = useState(false); 
+  const [currentFileId, setCurrentFileId] = useState<number>();
 
-  const addBook = useCallback(() => {}, []);
-  const addLocation = useCallback(() => {}, []);
+  const onSubmitAdd = useCallback(async(name: string, type: FileType) => {
+    await addBook(name, type, currentFileId);
+    queryClient.invalidateQueries('books');
+
+    setShowAddBookModal(false);
+    setShowAddLocationModal(false);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -32,16 +42,18 @@ function App() {
       <div className="flex flex-col gap-y-8 bg-slate-200 p-6 w-11/12 max-w-7xl rounded">
         <div className="flex w-full items-center gap-x-4">
           <Breadcrumbs paths={breadcrumbs} className="w-full" />
-          <Button onClick={addBook} className="whitespace-nowrap">
+          <Button onClick={() => setShowAddBookModal(true)} className="whitespace-nowrap">
             Add Book
           </Button>
-          <Button onClick={addLocation} className="whitespace-nowrap">
+          <Button onClick={() => setShowAddLocationModal(true)} className="whitespace-nowrap">
             Add Location
           </Button>
         </div>
-        <Directory/>
+        <Directory id={currentFileId}/>
       </div>
     </div>
+      <AddModal show={showAddBookModal} onSubmit={(name: string) => onSubmitAdd(name, FileType.Book)} onClose={() => setShowAddBookModal(false)}/>
+      <AddModal show={showAddLocationModal} onSubmit={(name: string) => onSubmitAdd(name, FileType.Location)} onClose={() => setShowAddLocationModal(false)}/>
     </QueryClientProvider>
 
   );
--- frontend/src/api/books.ts
+++ frontend/src/api/books.ts
@@ -1,10 +1,23 @@
 import axios, { AxiosError } from "axios";
 import { useQuery } from "react-query";
-import type { File } from "../containers/Directory/Directory";
+import type { File, FileType } from "../containers/Directory/Directory";
 
 const api_base_url = 'http://localhost:8081/api/v1';
 
 export type GetBooksResponse = File[];
+
+export async function addBook(name: string, type: FileType, parent?: number) {
+  const res = await axios.post<File>(
+    `${api_base_url}/books`,
+    {
+      name,
+      type,
+      parent
+    }
+  );
+
+  return res.data;
+}
 
 export async function getBooks(id?: number) {
   const res = await axios.get<GetBooksResponse>(`${api_base_url}/books/${id ?? ''}`);
